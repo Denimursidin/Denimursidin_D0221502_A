71d456ab7d5e9d1dc979c6a5a97ca13ebd2270d1
Denimursidin <99707692+Denimursidin@users.noreply.github.com>
2022-04-17 20:59:29 +0700

    Day58_Java_Sigleton_Pattern.java

---
+++ Day58_Java_Sigleton_Pattern.java
@@ -0,0 +1,19 @@
+# Denimursidin_D0221502_A
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+public String str;
+    private Singleton(){
+        
+    }
+    public static Singleton getSingleInstance(){
+        return new Singleton();    
+    }
+}
